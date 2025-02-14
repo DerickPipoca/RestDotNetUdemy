d671005ca89145c23fd635469ab794abef37f4b8
Dérick Pipoca <derickwfsilva@gmail.com>
2025-02-14 15:44:50 -0300

    Implementing calculator

---
+++ 01_01_RestDotNetUdemy_ScaffoldViaVisualStudio/RestDotNetUdemy/RestDotNetUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestDotNetUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNum}/{secondNum}")]
+        public IActionResult Get(string firstNum, string secondNum)
+        {
+            if (IsNumber(firstNum) && IsNumber(secondNum))
+            {
+                var sum = ConvertToDecimal(firstNum) + ConvertToDecimal(secondNum);
+                return Ok(sum);
+            }
+            return BadRequest("Invalid output!");
+        }
+
+        private static decimal ConvertToDecimal(string strNumber)
+        {
+            var result = decimal.TryParse(strNumber, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal number) ? number : 0;
+            return number;
+        }
+
+        private static bool IsNumber(string strNumber)
+        {
+            bool isNum = double.TryParse(strNumber, NumberStyles.Any, CultureInfo.InvariantCulture, out double number);
+            return isNum;
+        }
+    }
+}
